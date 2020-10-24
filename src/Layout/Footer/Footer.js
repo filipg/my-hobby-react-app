f30da980c9c1f3fe511f7afb0f58a45f66fb452d
Filip Gruczka <f.gruczka@actumlab.com>
2020-10-24 11:53:03 +0200

    Add base app styles

--- src/Layout/Footer/Footer.js
+++ src/Layout/Footer/Footer.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import classes from './Footer.module.scss';
 
 const footer = props => (
-    <h1>Footer</h1>
+    <div className={classes.Footer}>
+        {/*<h1>aa</h1>*/}
+    </div>
 )
 export default footer;
--- src/Layout/Header/Header.js
+++ src/Layout/Header/Header.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import classes from './Header.module.scss';
 
 const header = (props) => {
     return (
-        <div>Header</div>
+        <nav className={classes.Nav}>
+            <ul>
+                <li><a>Calendar</a></li>
+                <li><a>Friends</a></li>
+                <li><a>Hobby</a></li>
+                <li><a>Profile</a></li>
+            </ul>
+        </nav>
     )
 }
 export default header;
